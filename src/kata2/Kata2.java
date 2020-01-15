8bfac2eb0ab1d25c59dbd9ea1a59788e4e94448f
Luicko <Luicko@Ninita>
2020-01-15 16:08:03 +0000

    Second

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,16 @@
+package kata2;
+
+import java.util.Map;
+import kata2.Histogram;
+
+public class Kata2 {
+    public static void main(String[] args) {
+        
+        Histogram histo = new Histogram(new String[]{"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"});
+        Map<String,Integer> histogr = histo.getHistogram();
+            
+        for (String key : histogr.keySet()) {
+            System.out.println(key + " ==> " + histogr.get(key));
+        }
+    }  
+}
